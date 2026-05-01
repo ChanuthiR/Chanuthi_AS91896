188764f934b7285b4db7050d52bfb17f20d83bac
ChanuthiR <26352@students.mrgs.school.nz>
2026-05-01 12:34:38 +1200

    Countdown function created with different timers for each mode, and mode switch function developed,

--- focusTimer.py
+++ focusTimer.py
@@ -5,17 +5,15 @@ currentMode = "Focus"
 
 
 #initlizing timers for each mode in seconds
-focusTime = 20
-shortBreakTime = 5
-longBreakTime = 10
+focusTime = 10
+shortBreakTime = 2
+longBreakTime = 5
 
 #initalizing the session counter
 sessions = 0
 
 timeRemaining = 0 #initalizing time remaining variable
 timerDisplay = "" # timer display text initalized
-
-print("Timer started")
 
 #logic to select timer amount to match the mode
 
@@ -34,16 +32,19 @@ def switchMode():
     if sessions != 4:
         if currentMode == "Focus":
             currentMode = "Short Break"
+            sessions += 1
+            print("Session count: " + str(sessions)) #for testing that the session counter updates
         else:
             currentMode = "Focus"
     else:
-        currentMode = longBreakTime
+        currentMode = "Long Break"
         sessions = 0
-setTimer()
+
+
 
 #countdown function created
 while True:
-    while timeRemaining>0:
+    while timeRemaining!=0:
         timerDisplay = "{:02d}:{:02d}".format(timeRemaining//60, timeRemaining%60) #format timer display so it appears as 00:00
         time.sleep(1)
         timeRemaining-=1

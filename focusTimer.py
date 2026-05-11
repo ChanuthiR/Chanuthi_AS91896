145a62acb529a07b5606c1309fc4554af5ad7187
26352 <26352@students.mrgs.school.nz>
2026-05-12 09:51:53 +1200

    added mode buttons and added functionality, with active mode indicator and manual mode switch

--- focusTimer.py
+++ focusTimer.py
@@ -1,9 +1,6 @@
 import customtkinter as tk
-
-
 #import string tkinter variable
 from tkinter import StringVar
-
 
 #initalize mode
 currentMode = "Focus"
@@ -37,23 +34,10 @@ isTimerRunning = False
 timeRemaining = 0 #initalizing time remaining variable
 timerDisplayTxt = StringVar(root,value="00:00")# timer display text initalized as a Tkinter variable
 
-#creating focus button
-focusBtn = tk.CTkButton(root, text="Focus", fg_color="#2A2244", bg_color= "#160F37", text_color="#B776BB", font=tk.CTkFont(family="Consolas",size=20),border_spacing=10, corner_radius=20, width= 172, height=42,hover_color="#392E5E")
-focusBtn.pack()
-
-#creating short break button
-shortBreakBtn = tk.CTkButton(root, text="Short Break", fg_color="#2A2244", bg_color= "#160F37", text_color="#B776BB", font=tk.CTkFont(family="Consolas",size=20),border_spacing=10, corner_radius=20, width= 172, height=42,hover_color="#392E5E")
-shortBreakBtn.pack()
-
-#creating long break button
-longBreakBtn = tk.CTkButton(root, text="Long Break", fg_color="#2A2244", bg_color= "#160F37", text_color="#B776BB", font=tk.CTkFont(family="Consolas",size=20),border_spacing=10, corner_radius=20, width= 172, height=42,hover_color="#392E5E")
-longBreakBtn.pack()
-
 
 #creating Timer Display label
 timerDisplay = tk.CTkLabel(root, textvariable=timerDisplayTxt, text_color="#5DB69F", font=tk.CTkFont(family="Consolas", size=200),fg_color="#160F37")
 timerDisplay.pack( )
-
 
 
 #creating the start button with a hover effect and linking its functionality
@@ -67,20 +51,49 @@ def timerControl():
        startBtn.configure(text="Start")
        countdownTimer()
 
+#function for user to set the mode manually based on input from the buttons
+def setMode(newMode):
+    global  currentMode, isTimerRunning
+    currentMode = newMode
+    isTimerRunning=False
+    startBtn.configure(text="Start")
+    setTimer()
+
+
 
 startBtn = tk.CTkButton(root, text="Start", fg_color="#322952", bg_color= "#160F37", text_color="#FFFFFF", font=tk.CTkFont(family="Consolas",size=20), command=timerControl,border_spacing=10, corner_radius=20, width= 172, height=42,hover_color="#392E5E")
 startBtn.pack()
 
+#creating focus button
+focusBtn = tk.CTkButton(root, text="Focus", fg_color="#2A2244", bg_color= "#160F37", text_color="#B776BB", command=lambda: setMode("Focus"), font=tk.CTkFont(family="Consolas",size=20),border_spacing=10, corner_radius=20, width= 172, height=42,hover_color="#392E5E")
+focusBtn.pack()
 
-#logic to select timer amount to match the mode
+#creating short break button
+shortBreakBtn = tk.CTkButton(root, text="Short Break", fg_color="#2A2244", bg_color= "#160F37", text_color="#B776BB", command=lambda: setMode("Short Break"),font=tk.CTkFont(family="Consolas",size=20),border_spacing=10, corner_radius=20, width= 172, height=42,hover_color="#392E5E")
+shortBreakBtn.pack()
+
+#creating long break button
+longBreakBtn = tk.CTkButton(root, text="Long Break", fg_color="#2A2244", bg_color= "#160F37", text_color="#B776BB",command=lambda : setMode("Long Break"), font=tk.CTkFont(family="Consolas",size=20),border_spacing=10, corner_radius=20, width= 172, height=42,hover_color="#392E5E")
+longBreakBtn.pack()
+
+#logic to select timer amount to match the mode, and changes the display colours of the mode buttons to showcase active mode
 def setTimer():
    global currentMode, timeRemaining
    if currentMode == "Focus":
+       focusBtn.configure(fg_color="#744B77", text_color="#FFFFFF", hover_color="#905994")
+       shortBreakBtn.configure(fg_color="#2A2244", text_color="#B776BB", hover_color="#392E5E")
+       longBreakBtn.configure(fg_color="#2A2244", text_color="#B776BB", hover_color="#392E5E")
        timeRemaining = focusTime
    elif currentMode == "Short Break":
        timeRemaining = shortBreakTime
+       focusBtn.configure(fg_color="#2A2244", text_color="#B776BB", hover_color="#392E5E")
+       shortBreakBtn.configure(fg_color="#744B77",text_color="#FFFFFF", hover_color="#905994")
+       longBreakBtn.configure(fg_color="#2A2244", text_color="#B776BB", hover_color="#392E5E")
    elif currentMode == "Long Break":
        timeRemaining = longBreakTime
+       focusBtn.configure(fg_color="#2A2244", text_color="#B776BB", hover_color="#392E5E")
+       shortBreakBtn.configure(fg_color="#2A2244", text_color="#B776BB", hover_color="#392E5E")
+       longBreakBtn.configure(fg_color="#744B77",text_color="#FFFFFF", hover_color="#905994")
 
 
 #logic to switch mode based on the previous mode and the number of sessions completed
@@ -100,7 +113,6 @@ def switchMode():
 
 
 setTimer()
-
 
 #countdown function created
 def countdownTimer():
